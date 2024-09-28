fd75e0283b4f13141115a84300ce592873eb76ff
priyatham-k <priyatham7k@gmail.com>
2024-09-28 03:46:39 -0500

    updated model

--- models/Rental.js
+++ models/Rental.js
@@ -1,14 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const rentalSchema = new mongoose.Schema({
-  customerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  vehicleId: { type: mongoose.Schema.Types.ObjectId, ref: 'Vehicle' },
+  customerId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  vehicleId: { type: mongoose.Schema.Types.ObjectId, ref: "Vehicle" },
   rentalDuration: { type: Number, required: true },
   pickupAddress: { type: String, required: true },
   dropOffAddress: { type: String, required: true },
   deposit: { type: Number, required: true },
   insurance: { type: String, required: true },
   totalPrice: { type: Number, required: true },
+  make: { type: String, required: true },
+  model: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  make: { type: String, required: true },
+  model: { type: String, required: true },
+  pickupDate: { type: String, required: true },
+  returnDate: { type: String, required: true }
 });
 
-module.exports = mongoose.model('Rental', rentalSchema);
+module.exports = mongoose.model("Rental", rentalSchema);
